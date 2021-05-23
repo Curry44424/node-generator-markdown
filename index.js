2a2594e1bd3df3ef7dbe085c8c85bd94b5785caa
Curry44424 <75190573+Curry44424@users.noreply.github.com>
2021-05-22 23:57:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+const word = require()
+// TODO: Include packages needed for this application
+npm inquirer
+// TODO: Create an array of questions for user input
+const questions = [what is your name];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
