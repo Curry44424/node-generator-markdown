12574daaa4f9312eb611ad2efd50a626ad9b8718
Curry44424 <75190573+Curry44424@users.noreply.github.com>
2021-05-30 07:49:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-const inquirer = require("inquirer");
-const word = require()
-// TODO: Include packages needed for this application
-npm inquirer
-// TODO: Create an array of questions for user input
-const questions = [what is your name];
+const inquirer = require('inquirer');
+const generationMarkdown = require("./utils/generateMarkdown")
+const fs = require('fs');
+const path = require("path");
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions =
+[ 
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type: "input",
+            message: "what is the title for your project",
+            name: "Title",
+        },
+        {
 
-// Function call to initialize app
-init();
+            type: "input",
+            Message: "please give description of your project",
+            name: "Description",
+        },
+        {
+
+            type: "input",
+            message: "What is your Github username",
+            name: "userName",
+        },
+        {
+
+
+            type: "input",
+            name: "email",
+            Message: "what is your email",
+
+        },
+        {
+            type: "list",
+            name: "license",
+            Message: "what kind of license should your project have ",
+            choices: ["Apache 2.0", "MIT", "BSD 2.0", "CCO 2.0", "NONE"],
+        },
+        {
+            type: "input",
+            name: "test",
+            Message: "What commamd should be run to run test?",
+
+        },
+        {
+            type: "input",
+            name: "usage",
+            Message: "What does the user need to know to run test?",
+        },
+    ]
+
+
+function start() {
+  inquirer.prompt(questions).then( responses => {
+       console.log("generating readme")
+       writeToFile("README.MD", generateMarkdown({...responses}) )
+  })
+}       
+
+function writeToFile (filename , data){
+return fs.writeFileSync(path.join(process.cwd(), filename), data);
+}
+
+start();
